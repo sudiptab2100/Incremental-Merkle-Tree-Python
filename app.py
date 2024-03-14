60156ee48415184c42fcc428e68ee3d7f90dfb3e
Sudipta Basak <sudiptab2100@gmail.com>
2024-03-14 15:04:53 +0000

    making the set zero roots function private

--- app.py
+++ app.py
@@ -4,19 +4,14 @@ class MerkleTree:
     def __init__(self, depth):
         self.depth = depth
         self.zero_roots = list()
-        self.__set_zero_roots__()
+        self.__set_zero_roots()
     
     def hash_data(self, data=None):
         return sha256(data.encode() if data else bytes(0)).hexdigest()
     
-    def __set_zero_roots__(self):
-        t = None
-        t_hash = self.hash_data(t)
+    def __set_zero_roots(self):
+        t_hash = self.hash_data()
         self.zero_roots.append(t_hash)
         for i in range(1, self.depth):
             t_hash = self.hash_data(t_hash + t_hash)
             self.zero_roots.append(t_hash)
-
-
-m = MerkleTree(depth=20)
-print(m.zero_roots)
