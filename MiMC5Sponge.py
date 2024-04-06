b719c5a3efc9dc0d8f14c3fb5c0d93445e43bc21
Sudipta Basak <sudiptab2100@gmail.com>
2024-04-06 13:14:51 +0000

    MiMCSponge class implemented for number of rounds 20

--- MiMC5Sponge.py
+++ MiMC5Sponge.py
@@ -3,10 +3,12 @@ import json
 
 class MiMC5Sponge:
     def __init__(self):
-        self.nRounds = 20
-        self.p = 21888242871839275222246405745257275088548364400416034343698204186575808495617
-        with open('files/c_values.json') as fp:
-            self.c_values = json.load(fp)
+        with open('files/MiMC_settings.json') as fp:
+            micmcSettings = json.load(fp)
+        self.p = micmcSettings['p']
+        self.k = micmcSettings['k']
+        self.c_values = micmcSettings['c']
+        self.nRounds = micmcSettings['n']
     
     def __feistel(self, inL, inR, k):
         lastL, lastR = inL, inR
@@ -18,6 +20,11 @@ class MiMC5Sponge:
             lastL, lastR = lastR, (lastL + mask5) % self.p
         
         return lastL, lastR
-
-
-m = MiMC5Sponge()
+    
+    def hash(self, m):
+        lastR = lastC = 0
+        for i in range(len(m)):
+            lastR = (lastR + m[i]) % self.p
+            lastR, lastC = self.__feistel(lastR, lastC, self.k)
+        
+        return lastR
